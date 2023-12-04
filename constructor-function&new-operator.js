eebd430ee1e6a41de1ffe4935474e51c2198fe24
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-05 03:41:28 +0600

    Implement basic of constructor function.

--- constructor-function&new-operator.js
+++ constructor-function&new-operator.js
@@ -1,8 +1,16 @@
-// Here Person is a blueprint 
+// Here Person is a blueprint, constructor function.
 const Person=function(firstName, birthYear){
     // console.log(this); //Person {}
+
+    // instances properties
     this.fName= firstName;
     this.bYear= birthYear;
+
+    // NEVER USE A METHOD IN CONSTRUCTOR FUNCTION
+    // this.calAge= function(){
+    //     console.log(2024-birthYear);
+    // }
+    // SO, SOLVE IT BY USING PROTOTYPE
 };
 
 // new Person('sunny', 1998);
@@ -18,3 +26,6 @@ console.log(sunny); //Person { fName: 'sunny', bYear: 1998 }
 
 const jonas= new Person('jonas', 1990);
 console.log(jonas); //Person { fName: 'jonas', bYear: 1990 }
+
+// HERE, sunny and jonas are instance of Person.
+console.log(jonas instanceof Person); //true
