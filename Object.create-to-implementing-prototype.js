651ec1dd7f36969443f81a7796f4485b95025732
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-08 03:52:11 +0600

    Manually assigning Object.create method name & birthYear convert into a function.

--- Object.create-to-implementing-prototype.js
+++ Object.create-to-implementing-prototype.js
@@ -3,6 +3,11 @@
 const PersonProto={
     calAge(){
         console.log(2024-this.birthYear);
+    },
+
+    init(name, birthYear){
+        this.name= name;
+        this.birthYear= birthYear;
     }
 };
 
@@ -11,3 +16,16 @@ console.log(sunny);
 sunny.name='Sunny';
 sunny.birthYear=1998;
 sunny.calAge();
+
+console.log(sunny.__proto__);
+/**
+ {calAge: ƒ}
+   calAge: ƒ calAge()
+   [[Prototype]]: Object
+ */
+
+
+//////////////// Manually assigning name & birthYear convert into a function /////////////////
+const javaScript= Object.create(PersonProto);
+javaScript.init('Brendan Eich',1995 );
+javaScript.calAge(); //29
