5e05ab40e7fb34e890e90b916490ad1750cdbb15
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-07 13:24:28 +0600

    ES6 class to implement  prototype.

--- ES6-Classes-to-implementing-prototype.js
+++ ES6-Classes-to-implementing-prototype.js
@@ -11,10 +11,20 @@ class PersonClass {
         this.firstName= firstName;
         this.birthYear= birthYear;
     };
+    
     calAge(){
         console.log(2024-this.birthYear);
+    };
+
+    message(){
+        console.log(`Hey... I'm ${this.firstName}`);
     }
 }
 
 const sunny =new PersonClass('Sunny', 1998);
 sunny.calAge();
+sunny.message();
+  
+const siam=new PersonClass('Siam', 2006);
+siam.calAge();
+siam.message();
