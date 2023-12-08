f345e7fe2090073e02a13c1a356ebc08df7e21be
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-08 23:38:01 +0600

    Implement Inherit person parent class from student class -> Person.call(this keyword for newly created object, name, birthYear).

--- inheritance-between-classes.js
+++ inheritance-between-classes.js
@@ -10,7 +10,17 @@ Person.prototype.calAge= function(){
 };
 
 const Student= function(name, birthYear, dept){
-    this.name= name;
-    this.birthYear= birthYear;
+    // this.name= name;
+    // this.birthYear= birthYear;
+    Person.call(this, name, birthYear); //Inherit person parent class from student class -> Person.call(this keyword for newly created object, name, birthYear)
     this.dept= dept;
 };
+
+Student.prototype.introduceStudent= function(){
+    console.log(`My name is ${this.name} and my department is ${this.dept}`);
+};
+
+
+// create object
+const sunny= new Student('Sunny', 1998, 'CSE');
+sunny.introduceStudent();
