280c21d18bd59ee99eba967e9b66efc4a0190370
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-09 04:49:30 +0600

    If we comment accelerateMethod inside EV's prototype then tesla.accelerateMethod(); inherit on the accelerateMethod() which in the Car's prototype..

--- codingChallange3.js
+++ codingChallange3.js
@@ -12,13 +12,20 @@ Car.prototype.breakMethod = function () {
 };
 
 const EV = function (make, speed, charge) {
-  Car.call(this, make, speed); //Inside function expression,this keyword did not work.So use Car.call to call the Car constructor function.
+  Car.call(this, make, speed); //Inside function expression,this keyword did not work.So use Car.call to call the Car constructor function and use.
   this.charge = charge;
 };
 // link the prototype
 EV.prototype = Object.create(Car.prototype);
 EV.prototype.chargeBattery=function(chargeTo){
     this.charge=chargeTo;
+};
+
+// If we comment accelerateMethod inside EV's prototype then tesla.accelerateMethod(); inherit on the accelerateMethod() which in the Car's prototype.
+EV.prototype.accelerateMethod= function(){
+    this.speed += 20;
+    this.charge --;
+    console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`);
 };
 
 const bmw = new Car("BMW", 120);
@@ -28,3 +35,4 @@ const tesla = new EV("tesla", 150, 23);
 tesla.chargeBattery(90);
 console.log(tesla); //EV { make: 'tesla', speed: 150, charge: 23 }
 tesla.breakMethod();
+tesla.accelerateMethod();
