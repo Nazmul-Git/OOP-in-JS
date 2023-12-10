808ac1937c2e36dbc27097c479ec0ee02c036962
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-11 03:45:37 +0600

    Implement encapsulation/private method on movements array, can not access outside of the class.

--- encapsulation.js
+++ encapsulation.js
@@ -1,27 +1,40 @@
 class Account {
+    //public field
+    local= navigator.language;
+
+    // private field
+    #movements=[];
+
     constructor(owner, currency, pin){
         this.owner= owner;
         this.currency=currency;
-        this.pin=pin;
-        this.movements=[];
-        this.local=navigator.language;
+        this._pin=pin;
+        // this._movements=[];
+        // this.local=navigator.language;
         console.log(`Thanks for opening a new account, ${this.owner}`)
     }
 
+    // Implement more methods, each method can access other methods outside class.
+
+    // Public Interface
+    getMovements(){
+        return this.#movements;
+    }
+
     deposit(val){
-        this.movements.push(val);
+        this.#movements.push(val);
     }
 
     withdraw(val){
         this.deposit(-val)
     }
     
-    approveLoan(val){
+    _approveLoan(val){
         return true
     }
 
     requestLoan(val){
-        if(this.approveLoan(val)){
+        if(this._approveLoan(val)){
             this.deposit(val);
             console.log('Loan Approved')
         }
@@ -33,5 +46,7 @@ const acc1=new Account('Sunny', 'EUR', 1111);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000)
-acc1.approveLoan(1000)
+acc1._approveLoan(1000)
 console.log(acc1);
+console.log(acc1.getMovements());
+// console.log(acc1.#movements);//Uncaught SyntaxError: Private field '#movements' must be declared in an enclosing class
