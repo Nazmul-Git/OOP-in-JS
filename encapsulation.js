f02f48d3ca1f50d16481d53f66693c5d96dbbbbf
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-11 03:10:41 +0600

    Implement more 2 method, each method can access other methods inside class.

--- encapsulation.js
+++ encapsulation.js
@@ -15,10 +15,23 @@ class Account {
     withdraw(val){
         this.deposit(-val)
     }
+    
+    approveLoan(val){
+        return true
+    }
+
+    requestLoan(val){
+        if(this.approveLoan(val)){
+            this.deposit(val);
+            console.log('Loan Approved')
+        }
+    }
+
 };
 
 const acc1=new Account('Sunny', 'EUR', 1111);
 acc1.deposit(250);
 acc1.withdraw(140);
-
+acc1.requestLoan(1000)
+acc1.approveLoan(1000)
 console.log(acc1);
